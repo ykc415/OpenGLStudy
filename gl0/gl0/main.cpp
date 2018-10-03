3c55fb02d76473d88dcfebd8395bd76046a9ce05
ykc415 <pica415@gmail.com>
2018-10-03 20:46:26 +0900

    Added reshape callback

--- gl0/gl0/main.cpp
+++ gl0/gl0/main.cpp
@@ -14,9 +14,9 @@
 void MyDisplay() {
     
     glClear(GL_COLOR_BUFFER_BIT);
-    glViewport(0, 0, 300, 300);
+    glColor3f(0.5, 0.5, 0.5);
     glBegin(GL_POLYGON);
-    
+
     glVertex3d(-0.5, -0.5, 0.0);
     glVertex3d(0.5, -0.5, 0.0);
     glVertex3d(0.5, 0.5, 0.0);
@@ -26,17 +26,27 @@ void MyDisplay() {
     glFlush();
 }
 
+void MyReshape(int NewWidth, int NewHeight) {
+    glViewport(0, 0, NewWidth, NewHeight);
+    GLfloat WidthFactor = (GLfloat)NewWidth / (GLfloat)300;
+    GLfloat HeightFactor = (GLfloat)NewHeight / (GLfloat)300;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0 * WidthFactor, 1.0 * WidthFactor, -1.0 * HeightFactor, 1.0 * HeightFactor, -1.0, 1.0);
+}
+
 int main(int argc, char ** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
     glutInitWindowSize(300, 300);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL Drawing Example");
-    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     glutDisplayFunc(MyDisplay);
+    glutReshapeFunc(MyReshape);
     glutMainLoop();
     
     return 0;
