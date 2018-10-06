6f59f3d1a0ff357173c26b60d12ef5c3a6f11605
ykc415 <pica415@gmail.com>
2018-10-06 15:55:11 +0900

    Timer Example

---
+++ gl0/glExamples/timerExample.cpp
@@ -0,0 +1,53 @@
+////
+////  timerExample.cpp
+////  gl0
+////
+////  Created by SungHyunPark on 2018. 10. 6..
+////  Copyright © 2018년 SungHyunPark. All rights reserved.
+////
+//
+//#include <iostream>
+//#include <OpenGL/gl.h>
+//#include <OpenGl/glu.h>
+//#include <GLUT/glut.h>
+//
+//GLfloat Delta = 0.0;
+//
+//void MyDisplay() {
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    glBegin(GL_POLYGON);
+//    glColor3f(0.0, 0.5, 0.8);
+//    
+//    glVertex3f(-1.0 + Delta, -0.5, 0.0);
+//    glVertex3f(0.0 + Delta, -0.5, 0.0);
+//    glVertex3f(0.0 + Delta, 0.5, 0.0);
+//    glVertex3f(-1.0 + Delta, -0.5, 0.0);
+//    
+//    glEnd();
+//    glutSwapBuffers();
+//}
+//
+//void MyTimer(int Value) {
+//    Delta = Delta + 0.001;
+//    glutPostRedisplay();
+//    glutTimerFunc(40, MyTimer, 1);
+//}
+//
+//int main(int argc, char** argv) {
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+//    glutInitWindowSize(300, 300);
+//    glutInitWindowPosition(0, 0);
+//    glutCreateWindow("OpenGL Drawing Example");
+//    glClearColor(1.0, 1.0, 1.0, 1.0);
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    glOrtho(-1.0, 1.0, -1.0, 1.0, 1.0, -1.0);
+//    glutDisplayFunc(MyDisplay);
+//    glutTimerFunc(40, MyTimer, 1);
+//    glutMainLoop();
+//    return 0;
+//    
+//    
+//    
+//}
